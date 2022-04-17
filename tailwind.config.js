2ab7e296e368d91b99b7a41660d77bb2cecfa160
Joseph Ostrow <jstro@umich.edu>
2022-04-17 18:32:30 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [ './public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4169e1',
+      },
+      scale: {
+        '200': '2',
+        '250': '2.5',
+        '275': '2.75',
+      },
+      maxWidth: {
+       '1/4': '25%',
+       '1/2': '50%',
+       '3/4': '75%',
+      },
+      minWidth: {
+        '3/4': '75%',
+      }
+    },
+  },
+  plugins: [],
+}
